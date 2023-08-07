c69625227bb21d3cb1831d9cce25d2f6bfb2ca64
anshoo.mishra <anshmishra@Anshus-MacBook-Air.local>
2023-08-07 08:35:55 +0530

    docker issue of postgres solved

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import ToDoUser,Organization,OrganizationJoinRequest
 # Register your models here.
+
+admin.site.register(ToDoUser)
+admin.site.register(Organization)
+admin.site.register(OrganizationJoinRequest)

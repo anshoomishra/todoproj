d043516a1cc85c54b33e08d31ffcc2f10a8fa38d
anshoo.mishra <anshmishra@Anshus-MacBook-Air.local>
2023-09-24 21:15:09 +0530

    navigation home page working

--- todos/urls.py
+++ todos/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import ToDoHomeView,ToDoTaskCreateView,ToDoTaskUpdateView,ToDoListView,TaskDeleteView,TaskDetailView,TaskCompleteView, ToDoTaskCompleteView
 app_name = "todo"
 urlpatterns = [
-    path('',ToDoHomeView.as_view(),name='todo-home'),
+    path('',ToDoHomeView.as_view(),name='home'),
     path('task/create/',ToDoTaskCreateView.as_view(),name='todo-task-create'),
     path('task/<pk>/update/',ToDoTaskUpdateView.as_view(),name='todo-task-update'),
     path('tasks/',ToDoListView.as_view(),name='todo-tasks'),
